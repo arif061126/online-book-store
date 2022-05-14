33cd49ce3e242e8fdd413d5bf7d942600d72cb16
a.ahmed <a.ahmed@fz-juelich.de>
2022-05-14 19:11:28 +0200

    created repository config and change java version 11 to 1.8

---
+++ src/main/java/com/onlinebookstore/config/RepositoryConfig.java
@@ -0,0 +1,38 @@
+package com.onlinebookstore.config;
+
+import com.onlinebookstore.entity.Book;
+import com.onlinebookstore.entity.BookCategory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.Type;
+
+/**
+ * to expose the id in the json response
+ */
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+    @Autowired
+    private EntityManager entityManager;
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        /*
+        config.exposeIdsFor(Book.class);
+        config.exposeIdsFor(BookCategory.class);
+         */
+        //alternative way:
+        config.exposeIdsFor(entityManager
+                .getMetamodel()
+                .getEntities()
+                .stream()
+                .map(Type::getJavaType)
+                .toArray(Class[]::new)
+        );
+        //RepositoryRestConfigurer.super.configureRepositoryRestConfiguration(config, cors);
+    }
+}
--- src/main/java/com/onlinebookstore/repository/BookCategoryRepository.java
+++ src/main/java/com/onlinebookstore/repository/BookCategoryRepository.java
@@ -2,8 +2,10 @@ package com.onlinebookstore.repository;
 
 import com.onlinebookstore.entity.BookCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 
 //@Repository //no need for rest repository
+@RepositoryRestResource(collectionResourceRel = "bookCategories", path = "book-category")  //to change the endpoint of url
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
 }
